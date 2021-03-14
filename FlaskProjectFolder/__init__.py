e3d618c26464423a9a0cce00550f807e4d38e428
lildwagz <zamshit123@gmail.com>
2021-03-14 10:07:04 -0700

    routing auth controller

--- FlaskProjectFolder/__init__.py
+++ FlaskProjectFolder/__init__.py
@@ -2,7 +2,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 
 app = Flask(__name__)
 CORS(app)
--- FlaskProjectFolder/api/__init__.py
+++ FlaskProjectFolder/api/__init__.py
@@ -1,6 +1,6 @@
 # FlaskProjectFolder/api/__init__.py
 
-from flask import jsonify
+from flask import jsonify, make_response
 
 # Root Route Api
 from app import app
@@ -8,4 +8,30 @@ from app import app
 
 @app.route('/')
 def index():
-    return jsonify({"message": "Welcome to releaseskil Restfull-API"})
+    data = {
+        "list of available endpoints": [
+
+            {
+                "login":
+                    {"function": "to login your releaseskill account",
+                    "endpoint": "/auth/login",
+                    "method": "['GET','POST']",
+                    "usage": "use basic auth such as username and password",
+                    "payload" : "no"}
+
+            },
+            {
+                "register":{
+                    "function": "to create a new releaseskill account",
+                    "endpoint": "/auth/register",
+                    "method": "['POST']",
+                    "usage": "make sure you post the json payload with these keys (username,password,name,email,gender,"
+                             "phone_number)",
+                    "payload" : "yes"
+                }
+            }
+
+        ]
+    }
+    return jsonify({"message": "Welcome to releaseskil Restfull-API",
+                    "services": data})
